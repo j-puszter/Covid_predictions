52de675f66ebb85c8f244baf4b9cd3197a1f61fa
SingiSvk <73109512+SingiSvk@users.noreply.github.com>
2021-10-15 12:53:25 +0200

    added total death csv

--- data_processing/total_deaths.py
+++ data_processing/total_deaths.py
@@ -3,19 +3,27 @@ import plotly.express as px
 
 def main():
     data = load_data()
+    visualize(data)
+    make_csv(data, "TotalDeaths")
+
+
+# Visualization
+def visualize(df):
+    fig = px.line(df, x="Datum", y="Pocet.umrti",
+                  title="Vývoj celkového počtu úmrtí v súvislosti s COVID-19 na Slovensku")
+    fig.update_layout(xaxis_title="Dátum", yaxis_title="Počet úmrtí")
+    fig.show()
 
 
 def load_data():
     url = 'https://raw.githubusercontent.com/Institut-Zdravotnych-Analyz/covid19-data/main/DailyStats/OpenData_Slovakia_Covid_DailyStats.csv '
     df = pd.read_csv(url, delimiter=';')
 
-    # Visualization
-    fig = px.line(df, x="Datum", y="Pocet.umrti",
-                  title="Vývoj celkového počtu úmrtí v súvislosti s COVID-19 na Slovensku")
-    fig.update_layout(xaxis_title="Dátum", yaxis_title="Počet úmrtí")
-    fig.show()
-
     return df[["Datum", "Pocet.umrti"]]
+
+
+def make_csv(df, filename):
+    df.to_csv("csv/" + filename + ".csv", sep = ';', index = False)
 
 
 main()
