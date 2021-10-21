9926a329aaa55e006b33ceca7679884253dc8176
j-puszter <58809247+j-puszter@users.noreply.github.com>
2021-10-21 11:11:02 +0200

    pridanie obmedzenia zaciatocneho datumu

--- data_processing/AG_tests.py
+++ data_processing/AG_tests.py
@@ -10,8 +10,12 @@ def make_csv(df, filename):
 def get_AgTests():
     df = pd.read_csv("https://raw.githubusercontent.com/Institut-Zdravotnych-Analyz/covid19-data/main/DailyStats/OpenData_Slovakia_Covid_DailyStats.csv", sep=';', )
     ag_tests = df[["Datum", "AgPosit"]]
-    df["AgPosit%"] = round((df["AgPosit"] / df["AgTests"])*100, 1)
-    ag_tests.insert(2, "AgPosit%", df["AgPosit%"].values)
+
+    pd.to_datetime(ag_tests['Datum'])
+    ag_tests = ag_tests[(ag_tests['Datum'] >= '2020-10-11')]
+
+    # df["AgPosit%"] = round((df["AgPosit"] / df["AgTests"])*100, 1)
+    # ag_tests.insert(2, "AgPosit%", df["AgPosit%"].values)
     return ag_tests
 
 if __name__ == '__main__':
