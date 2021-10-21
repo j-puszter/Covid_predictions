11efa6648efdd9ea0d3f8ef2dd343fd7a77432e1
j-puszter <58809247+j-puszter@users.noreply.github.com>
2021-10-21 16:59:41 +0200

    odstranenie .0 z AgPosit

--- data_processing/AG_tests.py
+++ data_processing/AG_tests.py
@@ -8,16 +8,15 @@ def make_csv(df, filename):
     df.to_csv(filename + ".csv", sep = ';', index = False)
 
 def get_AgTests():
-    df = pd.read_csv("https://raw.githubusercontent.com/Institut-Zdravotnych-Analyz/covid19-data/main/DailyStats/OpenData_Slovakia_Covid_DailyStats.csv", sep=';', )
+    df = pd.read_csv("https://raw.githubusercontent.com/Institut-Zdravotnych-Analyz/covid19-data/main/DailyStats/OpenData_Slovakia_Covid_DailyStats.csv", sep=';')
     ag_tests = df[["Datum", "AgPosit"]]
 
     df["AgPosit%"] = round((df["AgPosit"] / df["AgTests"])*100, 1)
     df["AgPosit%"] = df["AgPosit%"].fillna(0)
     ag_tests.insert(2, "AgPosit%", df["AgPosit%"].values)
 
-    pd.to_datetime(ag_tests['Datum'])
     ag_tests = ag_tests[(ag_tests['Datum'] >= '2020-10-11')]
-
+    ag_tests.AgPosit = ag_tests.AgPosit.astype(int)
     return ag_tests
 
 if __name__ == '__main__':
